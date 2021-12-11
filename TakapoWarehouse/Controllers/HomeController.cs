24140ae86b92066eca1ec2c219d682e7a50f567d
vaqef.payfoon@outlook.com <vaqef.payfoon@outlook.com>
2021-12-12 00:37:47 +0800

    upload finish

--- TakapoWarehouse/Controllers/HomeController.cs
+++ TakapoWarehouse/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using Spire.Xls;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,6 +11,8 @@ using TakapoWarehouse.Data;
 using TakapoWarehouse.Models;
 using TakapoWarehouse.ViewModel;
 using System;
+using System.Data;
+using ExcelDataReader;
 
 namespace TakapoWarehouse.Controllers
 {
@@ -72,32 +73,65 @@ namespace TakapoWarehouse.Controllers
         [HttpPost, ActionName("Excel")]
         public IActionResult ExcelFileImport(SearchViewModel model)
         {
-            string webRootPath = _hostingEnvironment.WebRootPath;
+            //string webRootPath = _hostingEnvironment.WebRootPath;
 
-            if (HttpContext.Request.Form.Files.Count > 0)
+            //if (HttpContext.Request.Form.Files.Count > 0)
             {
-                var uploads = Path.Combine(webRootPath, "files");
-                var extension = Path.GetExtension(HttpContext.Request.Form.Files[0].FileName);
-                using (var filesStream = new System.IO.FileStream(Path.Combine(uploads, "excelFile" + extension), FileMode.Create))
+                //var uploads = Path.Combine(webRootPath, "files");
+                //var extension = Path.GetExtension(HttpContext.Request.Form.Files[0].FileName);
+                //using (var filesStream = new System.IO.FileStream(Path.Combine(uploads, "excelFile" + extension), FileMode.Create))
                 {
-                    Workbook workbook = new Workbook();
-                    workbook.LoadFromFile(Path.Combine(uploads, "newFile" + extension));
-                    Worksheet sheet = workbook.Worksheets[0];
-                    int rowCount = sheet.Rows.Length;
-                    int cellCount = sheet.Cells.Length;
+                    //HttpPostedFile Inputfile = null;
+                    //Workbook workbook = new Workbook();
+                    //workbook.LoadFromFile(@"C:\CSharp\TakapoWarehouse\TakapoWarehouse\wwwroot\files\newFile.xlsx");
+                    //Worksheet sheet = workbook.Worksheets[0];
+                    //int rowCount = sheet.Rows.Length;
+                    //int cellCount = sheet.Cells.Length;
 
-                    for(int row = 1; row < rowCount; row++)
+                    //for(int row = 2; row < rowCount; row++)
+                    //{
+                    //IngredientsWarehouse ingredient = new IngredientsWarehouse();
+                    //ingredient.Srl = Convert.ToInt32(sheet[row, 1].Text.Trim());
+                    //ingredient.IngredientsName = sheet[row, 2].Text.Trim();
+                    //ingredient.PartNo = sheet[row, 3].Text.Trim();
+                    //ingredient.Barcode = sheet[row, 4].Text.Trim();
+                    //ingredient.GoodsModel = sheet[row, 5].Text.Trim();
+                    //ingredient.StockIngredients = sheet[row, 6].Text.Trim();
+                    //ingredient.Description = sheet[row, 8].Text.Trim();
+                    //_db.IngredientsWarehouses.Add(ingredient);
+                    //_db.SaveChanges();
+                    //}
+                }
+            }
+
+            DataSet dsexcelRecords = new DataSet();
+            IExcelDataReader reader = null;
+
+            using (var filesStream = new FileStream(Path.Combine(@"C:\CSharp\TakapoWarehouse\TakapoWarehouse\wwwroot\files\warehouse.xls"), FileMode.Open))
+            {
+                if (filesStream != null)
+                {
+                    reader = ExcelReaderFactory.CreateBinaryReader(filesStream);
+                    dsexcelRecords = reader.AsDataSet();
+                    reader.Close();
+
+                    if (dsexcelRecords != null && dsexcelRecords.Tables.Count > 0)
                     {
-                        IngredientsWarehouse ingredient = new IngredientsWarehouse();
-                        ingredient.Srl = Convert.ToInt32(sheet[row, 1].Text);
-                        ingredient.IngredientsName = sheet[row, 2].Text;
-                        ingredient.PartNo = sheet[row, 3].Text;
-                        ingredient.Barcode = sheet[row, 4].Text;
-                        ingredient.GoodsModel = sheet[row, 5].Text;
-                        ingredient.StockIngredients = sheet[row, 6].Text;
-                        ingredient.Description = sheet[row, 8].Text;
-                        _db.IngredientsWarehouses.Add(ingredient);
-                        _db.SaveChanges();
+                        DataTable dtIngredients = dsexcelRecords.Tables[0];
+                        for (int i = 1; i < dtIngredients.Rows.Count; i++)
+                        {
+                            IngredientsWarehouse ingredient = new IngredientsWarehouse();
+                            ingredient.Srl = Convert.ToInt32(dtIngredients.Rows[i][0]);
+                            ingredient.IngredientsName = dtIngredients.Rows[i][1].ToString().Trim();
+                            ingredient.PartNo = dtIngredients.Rows[i][2].ToString().Trim();
+                            ingredient.Barcode = dtIngredients.Rows[i][3].ToString().Trim();
+                            ingredient.GoodsModel = dtIngredients.Rows[i][4].ToString().Trim();
+                            ingredient.StockIngredients = dtIngredients.Rows[i][5].ToString().Trim();
+                            ingredient.Description = dtIngredients.Rows[i][7].ToString().Trim();
+                            _db.IngredientsWarehouses.Add(ingredient);
+                            _db.SaveChanges();
+                        }
+
                     }
                 }
             }
